2ff541a55c90b84786413a015a9aaf1353ff1805
santi <stecuanhuey7@gmail.com>
2025-02-26 03:33:56 +0000

    Lab 7 - Python Program

---
+++ stecuanhueygarcia7.py
@@ -0,0 +1,28 @@
+# Santiago Tecuanhuey Garcia 
+# Lab 7 - Python 
+
+theInt = 7
+theFloat = 17.38
+theString = "Hello World!"
+
+if theString == "helloWorld":
+    print("The variable theString is helloWorld")
+else:
+    print("The variable theString is not Hello World!")
+
+if theInt < 5:
+    print("theInt variable is less than 5.")
+elif theInt > 10:
+    print("theInt variable is greater than 10.")
+else:
+    print("theInt variable is between 5 and 10.")
+
+if theFloat < 5.5:
+    print("theFloat variable is less than 5.5")
+elif theFloat > 20.5:
+    print("theFloat variable is greater than 20.5")
+elif theFloat == 17.38:
+    print("theFloat variable is equal to 17.38")
+else:
+    ("theFloat variable is not within range.")
+
